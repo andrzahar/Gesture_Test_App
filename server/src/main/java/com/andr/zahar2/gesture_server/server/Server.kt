6b1a7305b41444b455be6446034b24189b21efe3
andrz <andr.zahar2@gmai.com>
2024-06-13 03:11:25 +0500

    Server module: add api Constants usage in Server

--- server/src/main/java/com/andr/zahar2/gesture_server/server/Server.kt
+++ server/src/main/java/com/andr/zahar2/gesture_server/server/Server.kt
@@ -1,15 +1,15 @@
 package com.andr.zahar2.gesture_server.server
 
-import android.util.Log
 import com.andr.zahar2.api.model.ClientEvent
+import com.andr.zahar2.api.model.Constants
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.websocket.WebSockets
-import io.ktor.serialization.kotlinx.*
 import io.ktor.server.routing.routing
+import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.receiveDeserialized
 import io.ktor.server.websocket.webSocket
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureWebSockets(connectionsManager: ClientsConnectionsManager) {
     install(WebSockets) {
@@ -17,19 +17,16 @@ fun Application.configureWebSockets(connectionsManager: ClientsConnectionsManage
     }
 
     routing {
-        webSocket("/gestures") {
+        webSocket(Constants.PATH) {
             connectionsManager.addConnection(this)
-            Log.d("ttt", "newConnection")
             try {
                 while (true) {
                     val clientEvent = receiveDeserialized<ClientEvent>()
-                    connectionsManager.onClientEvent(clientEvent)
+                    connectionsManager.onClientEvent(this, clientEvent)
                 }
-            } catch (e: Exception) {
-                Log.d("ttt", e.message ?: "no message")
+            } catch (_: Exception) {
             } finally {
                 connectionsManager.removingConnection(this)
-                Log.d("ttt", "removed connection")
             }
         }
     }
