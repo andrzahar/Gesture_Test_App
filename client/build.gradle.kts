7e0017bb14e2f44a35a312e38c1f325c8d65dabd
andrz <andr.zahar2@gmai.com>
2024-06-12 23:17:26 +0500

    Client module: add datastore and viewmodel libraries

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -73,6 +73,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.viewmodel)
+
+    implementation(libs.datastore)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
