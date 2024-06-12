d127399a324bdd43f908e9e6535c2257e7d7f684
andrz <andr.zahar2@gmai.com>
2024-06-13 04:41:46 +0500

    Server module: add ContentView

---
+++ server/src/main/java/com/andr/zahar2/gesture_server/ui/ContentView.kt
@@ -0,0 +1,88 @@
+package com.andr.zahar2.gesture_server.ui
+
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.andr.zahar2.gesture_server.R
+
+@Composable
+fun ContentView(
+    modifier: Modifier,
+    viewModel: MainViewModel = hiltViewModel()
+) {
+
+    var openDialog by remember { mutableStateOf(false) }
+
+
+    Box(
+        modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragEnd = viewModel::onDrugEnd,
+                    onDrag = viewModel::onDrag
+                )
+            }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(onClick = { openDialog = true }) {
+                    Text(text = stringResource(id = R.string.config_button))
+                }
+                Button(onClick = { }) {
+                    Text(text = stringResource(R.string.logs_button))
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(onClick = viewModel::onStartClick) {
+                    Text(text = stringResource(id = R.string.start_button))
+                }
+                Button(onClick = viewModel::onStopClick) {
+                    Text(text = stringResource(R.string.stop_button))
+                }
+            }
+        }
+    }
+
+    if (openDialog) {
+        ConfigDialog {
+            openDialog = false
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ContentViewPreview() {
+    ContentView(modifier = Modifier)
+}
