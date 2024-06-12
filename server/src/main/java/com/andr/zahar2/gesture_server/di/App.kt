e0cdddf07ca070f396d530fb955aee8dd0f87c0a
andrz <andr.zahar2@gmai.com>
2024-06-13 03:22:52 +0500

    Server module: add DI

---
+++ server/src/main/java/com/andr/zahar2/gesture_server/di/App.kt
@@ -0,0 +1,7 @@
+package com.andr.zahar2.gesture_server.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
---
+++ server/src/main/java/com/andr/zahar2/gesture_server/di/ApplicationModule.kt
@@ -0,0 +1,36 @@
+package com.andr.zahar2.gesture_server.di
+
+import android.content.Context
+import com.andr.zahar2.api.model.GestureEvent
+import com.andr.zahar2.gesture_server.domain.GestureDomain
+import com.andr.zahar2.gesture_server.server.ClientsConnectionsManager
+import com.andr.zahar2.gesture_server.server.ServerManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideClientsConnectionsManager(): ClientsConnectionsManager = ClientsConnectionsManager()
+
+    @Provides
+    @Singleton
+    fun provideServerManager(
+        @ApplicationContext appContext: Context,
+        clientsConnectionsManager: ClientsConnectionsManager
+    ): ServerManager = ServerManager(appContext, clientsConnectionsManager)
+
+    @Provides
+    @Singleton
+    fun provideGestureDomain(
+        serverManager: ServerManager,
+        clientsConnectionsManager: ClientsConnectionsManager
+    ): GestureDomain = GestureDomain(serverManager, clientsConnectionsManager)
+}
