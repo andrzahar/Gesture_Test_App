354d594fcef625972a98ee02080a569680c3c6c1
andrz <andr.zahar2@gmai.com>
2024-06-13 04:41:20 +0500

    Server module: MainViewModel: add logic for gesture

--- server/src/main/java/com/andr/zahar2/gesture_server/ui/MainViewModel.kt
+++ server/src/main/java/com/andr/zahar2/gesture_server/ui/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.andr.zahar2.gesture_server.ui
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.lifecycle.ViewModel
+import com.andr.zahar2.api.model.GestureEvent
 import com.andr.zahar2.gesture_server.domain.GestureDomain
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,12 +11,36 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(private val domain: GestureDomain): ViewModel() {
 
+    private var startPointer: PointerInputChange? = null
+    private var endPointer: PointerInputChange? = null
+
     fun onStartClick() {
         domain.start()
     }
 
     fun onStopClick() {
         domain.stop()
+    }
+
+    fun onDrag(change: PointerInputChange, dragAmount: Offset) {
+        if (startPointer == null) {
+            startPointer = change
+        } else {
+            endPointer = change
+        }
+    }
+
+    fun onDrugEnd() {
+        if (startPointer != null && endPointer != null) {
+            val gestureEvent = GestureEvent(
+                startPoint = startPointer!!.position.toPoint(),
+                endPoint = endPointer!!.position.toPoint(),
+                duration = endPointer!!.uptimeMillis - startPointer!!.uptimeMillis
+            )
+            domain.onGestureEvent(gestureEvent)
+        }
+        startPointer = null
+        endPointer = null
     }
 
     override fun onCleared() {
