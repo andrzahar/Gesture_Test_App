a0e11c0de3e323664a3bf78efacf0ba73fb1e845
andrz <andr.zahar2@gmai.com>
2024-06-13 07:21:21 +0500

    Server module: add LogItem

---
+++ server/src/main/java/com/andr/zahar2/gesture_server/ui/log/LogItem.kt
@@ -0,0 +1,37 @@
+package com.andr.zahar2.gesture_server.ui.log
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.andr.zahar2.api.model.ClientEvent
+import com.andr.zahar2.gesture_server.data.model.LogEvent
+import java.util.Date
+
+@Composable
+fun LogItem(logEvent: LogEvent) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween
+        ) {
+            Text(text = logEvent.address)
+            Text(text = logEvent.event.name)
+        }
+        Text(text = Date(logEvent.timestamp).toLocaleString())
+    }
+}
+
+@Preview
+@Composable
+fun LogItemPreview() {
+    LogItem(logEvent = LogEvent(
+        timestamp = 1718243080473,
+        address = "192.168.101.6",
+        event = ClientEvent.CHROME_IS_OPEN
+    ))
+}
