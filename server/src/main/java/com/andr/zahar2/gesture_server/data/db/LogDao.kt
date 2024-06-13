6a9a1d29953a6b8e82e0979f06f607fb32325b26
andrz <andr.zahar2@gmai.com>
2024-06-13 06:54:16 +0500

    Server module: LogDao: add OnConflictStrategy

--- server/src/main/java/com/andr/zahar2/gesture_server/data/db/LogDao.kt
+++ server/src/main/java/com/andr/zahar2/gesture_server/data/db/LogDao.kt
@@ -2,6 +2,7 @@ package com.andr.zahar2.gesture_server.data.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.andr.zahar2.gesture_server.data.model.LogEvent
 import kotlinx.coroutines.flow.Flow
@@ -12,7 +13,7 @@ interface LogDao {
     @Query("SELECT * FROM logevent")
     fun getAll(): Flow<List<LogEvent>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(vararg logEvents: LogEvent)
 
     @Query("DELETE FROM logevent")
