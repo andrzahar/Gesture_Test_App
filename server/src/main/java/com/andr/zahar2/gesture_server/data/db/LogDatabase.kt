b41674057eec6a0d1dfeab9250122687d949b484
andrz <andr.zahar2@gmai.com>
2024-06-13 06:22:56 +0500

    Server module: fix LogDatabase

--- server/src/main/java/com/andr/zahar2/gesture_server/data/db/LogDatabase.kt
+++ server/src/main/java/com/andr/zahar2/gesture_server/data/db/LogDatabase.kt
@@ -1,9 +1,10 @@
 package com.andr.zahar2.gesture_server.data.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.andr.zahar2.gesture_server.data.model.LogEvent
 
 @Database(entities = [LogEvent::class], version = 1)
-abstract class LogDatabase {
+abstract class LogDatabase: RoomDatabase() {
     abstract fun logDao(): LogDao
 }
